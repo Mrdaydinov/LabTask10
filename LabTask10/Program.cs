b5ad7fa95bf8a430c34822015c1f204e2d410d6b
Murad <mrdaydinov@gmail.com>
2024-03-14 12:58:29 +0400

    task 3

--- LabTask10/Program.cs
+++ LabTask10/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LabTask10
+﻿using System.Xml.Linq;
+
+namespace LabTask10
 {
     internal class Program
     {
@@ -16,32 +18,94 @@
             //////
 
 
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            int[] arr1 = new int[arr.Length / 3];
-            int[] arr2 = new int[arr.Length / 3];
-            int[] arr3 = new int[arr.Length / 3];
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //int[] arr1 = new int[arr.Length / 3];
+            //int[] arr2 = new int[arr.Length / 3];
+            //int[] arr3 = new int[arr.Length / 3];
 
-            int index = 0;
-            
-            for (int i = 2; i < arr.Length; i += 3)
+            //int index = 0;
+
+            //for (int i = 2; i < arr.Length; i += 3)
+            //{
+            //    arr3[index] = arr[i];
+            //    arr2[index] = arr[i-1];
+            //    arr1[index] = arr[i-2];
+            //    index++;
+            //}
+
+            //Console.WriteLine("Arr1 = ");
+            //foreach (int i in arr1)
+            //    Console.WriteLine(i);
+
+            //Console.WriteLine("Arr2 = ");
+            //foreach (int i in arr2)
+            //    Console.WriteLine(i);
+
+            //Console.WriteLine("Arr3 = ");
+            //foreach (int i in arr3)
+            //    Console.WriteLine(i);
+
+
+            ///////
+
+
+            string[] names = { "Vahid", "Nicat", "Gülər", "Gözəl", "Ulduz", "Famil", "Dadaş", "Vagif", "Ümid", "Durna" };
+            int count = 0;
+
+            foreach (string name in names)
             {
-                arr3[index] = arr[i];
-                arr2[index] = arr[i-1];
-                arr1[index] = arr[i-2];
-                index++;
+                foreach (char c in name)
+                    if (c == 'u')
+                        count++;
             }
 
-            Console.WriteLine("Arr1 = ");
-            foreach (int i in arr1)
-                Console.WriteLine(i);
+            string[] newArr = new string[count];
+            int index = 0;
+            foreach (string name in names)
+            {
+                bool flag = false;
+                foreach (char c in name)
+                {
+                    if (c == 'u')
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
 
-            Console.WriteLine("Arr2 = ");
-            foreach (int i in arr2)
-                Console.WriteLine(i);
+                if (flag)
+                {
+                    newArr[index] = name;
+                    index++;
+                }
 
-            Console.WriteLine("Arr3 = ");
-            foreach (int i in arr3)
-                Console.WriteLine(i);
+            }
+
+            foreach (string name in newArr)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("===========");
+
+
+            string[] str = new string[count];
+
+            foreach (string name in newArr)
+            {
+                char[] charArr = name.ToArray();
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    if (charArr[i] == 'u')
+                    {
+                        charArr[i] = 'e';
+                    }
+                }
+
+                Console.WriteLine(charArr);
+            }
+
+
         }
     }
 }
