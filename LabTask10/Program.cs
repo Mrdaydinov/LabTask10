b2829d23f4fb3b8711074271b2e5b23a8fd17f76
Murad <mrdaydinov@gmail.com>
2024-03-14 11:25:49 +0400

    init commit

---
+++ LabTask10/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace LabTask10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int n = 15;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i*n);
+            //}
+
+
+            //////
+            
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            int[] arr1 = new int[arr.Length / 3];
+            int[] arr2 = new int[arr.Length / 3];
+            int[] arr3 = new int[arr.Length / 3];
+
+            int index = 0;
+            for (int i = 0; i < arr.Length; i += 3)
+            {
+                arr1[index] = arr[i];
+                index++;
+            }
+            index = 0;
+            for (int i = 1; i < arr.Length; i += 3)
+            {
+                arr2[index] = arr[i];
+                index++;
+            }
+            index = 0;
+            for (int i = 2; i < arr.Length; i += 3)
+            {
+                arr3[index] = arr[i];
+                index++;
+            }
+
+            Console.WriteLine("Arr1 = ");
+            foreach (int i in arr1)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Arr2 = ");
+            foreach (int i in arr2)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Arr3 = ");
+            foreach (int i in arr3)
+                Console.WriteLine(i);
+        }
+    }
+}
